0c9e242272bb6af0526e59985c0a21aa4bfcf859
Chetan Yadav <ydvchetan01@gmail.com>
2024-01-02 12:00:32 +0530

    Create Comment.java & Main.java

---
+++ Comment.java
@@ -0,0 +1,23 @@
+public class Comment {
+  public static void main(String[] args) {
+    System.out.println("Chetan Yadav");
+    System.out.println("Ips Academy Indore");
+    System.out.println("Cse");
+
+    // System.out.println(India); Error
+
+    System.out.println(5);
+    System.out.println(50);
+    System.out.println(100);
+    System.out.println(3 + 1);
+    System.out.println(3 * 1);
+
+    System.out.print("Hello World! ");
+    System.out.print("Lakhan Yadav ");
+
+    // Single Line Comment
+    System.out.println("Dutt Nagar, Indore"); // Address
+
+    /* Multi Line Comment --> Comments can be used to explain Java code, and to make it more readable. It can also be used to prevent execution when testing alternative code. */
+  }
+}
