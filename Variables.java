ec1e08953c9086308d7e6efb89cb8645e21b13de
Chetan Yadav <ydvchetan01@gmail.com>
2024-01-03 11:48:05 +0530

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,49 @@
+public class Variables {
+  public static void main(String[] args) {
+    // type variableName = value;
+    String name = "Chetan";
+    System.out.println(name);
+
+    int number = 724;
+    System.out.println(number);
+
+    int num;
+    num = 14;
+    System.out.println(num);
+
+    int numm = 17;
+    numm = 15;
+    System.out.println(numm);
+
+    final int myNum = 13;
+    // myNum = 20; Error
+
+    int mNum = 5;
+    float myFloatNum = 5.99f;
+    char myLetter = 'D';
+    boolean myBool = true;
+    String myText = "Hello";
+
+    String namee = "Chetan";
+    System.out.println("Hello " + namee);
+
+    String firstName = "Chetan ";
+    String lastName = "Yadav";
+    String fullName = firstName + lastName;
+    System.out.println(fullName);
+
+    int x = 6;
+    int y = 7;
+    System.out.println(x + y);
+
+    int a = 1, b = 2, c = 3;
+    System.out.println(a + b + c);
+
+    int p, q, r;
+    p = q = r = 8;
+    System.out.println(p + q + r);
+
+    int numofStudents = 60;
+    // int m = 60;
+  }
+}
