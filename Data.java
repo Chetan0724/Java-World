8fccc9d3c30b1b6b0801ec13e06b7852cd2f9db8
Chetan Yadav <ydvchetan01@gmail.com>
2024-01-04 13:12:34 +0530

    Create Data.java

---
+++ Data.java
@@ -0,0 +1,40 @@
+public class Data {
+  public static void main(String[] args) {
+    byte number1 = 100;
+    System.out.println(number1);
+
+    short number2 = 30000;
+    System.out.println(number2);
+
+    int number3 = 100000;
+    System.out.println(number3);
+
+    long number4 = 15000000000L;
+    System.out.println(number4);
+
+    float number5 = 5.59f;
+    System.out.println(number5);
+
+    double number6 = 12.58d;
+    System.out.println(number6);
+
+    float f1 = 35e3f;
+    double d1 = 12E4d;
+    System.out.println(f1);
+    System.out.println(d1);
+
+    boolean drive = false;
+    System.out.println(drive);
+
+    char letter = 'C';
+    System.out.println(letter);
+
+    char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+    System.out.println(myVar1);
+    System.out.println(myVar2);
+    System.out.println(myVar3);
+
+    String name = "Chetan";
+    System.out.println(name);
+  }
+}
