b575b25fd18749bc68dad2dd43f6c2614020d3e8
Chetan Yadav <ydvchetan01@gmail.com>
2024-01-05 12:30:05 +0530

    Create Typecasting.java

---
+++ Typecasting.java
@@ -0,0 +1,15 @@
+public class Typecasting {
+  public static void main(String[] args) {
+    // Widening Casting
+    int myInt = 7;
+    double myDouble = myInt;
+    System.out.println(myInt);
+    System.out.println(myDouble);
+
+    // Narrowing Casting
+    double myDoub = 6.74d;
+    int myIn = (int) myDoub;
+    System.out.println(myDoub);
+    System.out.println(myIn);
+  }
+}
