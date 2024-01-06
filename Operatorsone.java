86315e6bcb767994c7cf0197f2539a68a58469d4
Chetan Yadav <ydvchetan01@gmail.com>
2024-01-06 15:03:34 +0530

    Create Operatorsone.java

---
+++ Operatorsone.java
@@ -0,0 +1,27 @@
+public class Operatorsone {
+  public static void main(String[] args) {
+    int x = 200 + 500;
+    System.out.println(x);
+    int y = x + 100;
+    System.out.println(y);
+    int z = x + y;
+    System.out.println(z);
+    int k = z + z;
+    System.out.println(k);
+
+    // Arithmetic Operators
+    int num1 = 12;
+    int num2 = 3;
+    System.out.println(num1 + num2);
+    System.out.println(num1 - num2);
+    System.out.println(num1 * num2);
+    System.out.println(num1 / num2);
+    System.out.println(num1 % num2);
+
+    int num3 = 4;
+    ++num3;
+    System.out.println(num3);
+    --num3;
+    System.out.println(num3);
+  }
+}
