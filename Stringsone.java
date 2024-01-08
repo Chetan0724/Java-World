c9681f65e29a9246eb94c3eb97fcad89f6a922eb
Chetan Yadav <ydvchetan01@gmail.com>
2024-01-08 09:59:17 +0530

    String Concatenation

---
+++ Stringsone.java
@@ -0,0 +1,12 @@
+public class Stringsone {
+  public static void main(String[] args) {
+    // String Concatenation
+    String firstName = "Chetan";
+    String lastName = "Yadav";
+    System.out.println(firstName + " " + lastName);
+
+    String firstNameone = "Lakhan ";
+    String lastNameone = "Yadav";
+    System.out.println(firstNameone.concat(lastNameone));
+  }
+}
