41f34038c8a25b73e490269fbe30bfb5f728fc47
Chetan Yadav <ydvchetan01@gmail.com>
2024-01-10 13:51:12 +0530

    Math Class Methods

---
+++ MyMath.java
@@ -0,0 +1,12 @@
+public class MyMath {
+  public static void main(String[] args) {
+    System.out.println(Math.max(5, 2));
+    System.out.println(Math.min(5, 2));
+    System.out.println(Math.sqrt(25));
+    System.out.println(Math.abs(-2.3));
+    System.out.println(Math.random());
+
+    int randomNum = (int)(Math.random() * 101);
+    System.out.println(randomNum);
+  }
+}
